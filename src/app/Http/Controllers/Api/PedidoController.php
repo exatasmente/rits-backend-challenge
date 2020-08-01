5523f5398f128cc6c65d811a8853f05aa791b14d
exatasmente <lu_u_z@hotmail.com>
2020-08-01 20:44:49 -0300

    Cliente api curd completed

--- src/app/Http/Controllers/Api/PedidoController.php
+++ src/app/Http/Controllers/Api/PedidoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PedidoClienteRequest;
+use App\Http\Requests\PedidoDestroyRequest;
+use App\Http\Requests\PedidoStoreRequest;
 use App\Services\ClienteService;
 use App\Services\PedidoService;
 use Illuminate\Support\Facades\Auth;
@@ -15,14 +17,18 @@ class PedidoController extends Controller
     public function index(ClienteService $clientes){
         return $clientes->find(Auth::user()->id)->pedidos;
     }
+    public function show(PedidoService $pedidos,ClienteService $clientes,$pedidoId){
+        $cliente =  $clientes->find(Auth::user()->id);
+        return $pedidos->find(optional($cliente->pedidos()->find($pedidoId))->id);
 
-    public function store(PedidoClienteRequest $request,PedidoService $pedidos){
+    }
+    public function store(PedidoStoreRequest $request,PedidoService $pedidos){
         return $pedidos->createPedido(Auth::user()->id,$request->get('produtos'));
     }
 
-    public function destroy(PedidoService $pedidos,$pedidoId)
+    public function destroy(PedidoDestroyRequest $request,PedidoService $pedidos)
     {
-      return $pedidos->destroy($pedidoId);
+      return $pedidos->destroy($request->input());
     }
 
 }
---
+++ src/app/Http/Requests/PedidoDestroyRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PedidoDestroyRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'pedido' => 'required|exists:pedidos,id',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'pedido.exists' => 'O Pedido não existe',
+            'pedido.required' => 'O Pedido é obrigatório'
+        ];
+    }
+}
